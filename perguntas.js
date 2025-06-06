753467b7cfeab3b5179ec11603a8fc0eb31860d5
oclavo <141341283+oclavo@users.noreply.github.com>
2025-06-06 08:00:12 -0300

    d

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Academia',
+    'O que é pulldown?',
+    'Pesquise e volte aqui'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a finalidade da variável em javascript?',
+    'Armazenar valores aleatórios a serem transcritos no html'
+)
+
+
+criaCartao(
+    'Programação',
+    'mundial do palmeiras?',
+    'não existe'
+)
